deb089336e569f0b1367e7a29a26682650033ca3
michael <michael.cashmore@kcl.ac.uk>
2019-04-01 17:48:38 +0100

    Updated domain and replan loop, added launcher

--- rosplan_planning_system/src/STN_tool/replan_loop.py
+++ rosplan_planning_system/src/STN_tool/replan_loop.py
@@ -3,6 +3,7 @@ import rospkg
 import rospy
 import sys
 import time
+import os
 
 from std_srvs.srv import Empty, EmptyResponse
 from rosplan_knowledge_msgs.srv import SetInt
@@ -10,13 +11,15 @@ from rosplan_dispatch_msgs.srv import DispatchService, DispatchServiceResponse
 
 # get path of pkg
 rospack = rospkg.RosPack()
+rospy.init_node("coordinator_non_adaptable")
 
-problem_path = rospy.get_param('~/rosplan_STN_tool/problem_path', "problem.pddl")
-experiment_batch = rospy.get_param('~/coordinator_non_adaptable/experiment_batch', '0')
+problem_path = rospy.get_param('/coordinator_non_adaptable/problem_path', "problem.pddl")
+experiment_batch = rospy.get_param('/coordinator_non_adaptable/experiment_batch', '0')
 
 
 goal_achieved = False
 replans = 0
+snap = time.time()
 while not goal_achieved and replans<10:
     rospy.wait_for_service('/rosplan_problem_interface/problem_generation_server')
     rospy.wait_for_service('/rosplan_planner_interface/planning_server')
@@ -57,11 +60,10 @@ while not goal_achieved and replans<10:
         print "Service call failed: %s"%e
         replans += 1
 
-if goal_achieved:
-    print "SUCCESS ", str(replans)
-    result = open(problem_path[:-5] + '_result_batch' +str(experiment_batch) +'.txt', 'w')
-    result.write('SUCCESS  '+ str(replans) + '\n')
-else:
-    print "FAILED ", str(replans)
-    result = open(problem_path[:-5] + '_result_batch' + str(experiment_batch) +'.txt', 'w')
-    result.write('FAILED  '+ str(replans) + '\n')
+file_path = "~/" + os.path.basename(problem_path)[:-5] + '_result_batch' +str(experiment_batch) +'.txt'
+elapsed = time.time() - snap
+elapsed_sim = rospy.get_time()
+success = "SUCCESS" if goal_achieved else "FAILED"
+with open(file_path, 'wt') as result:
+    result.write("%s, %f, %f, %s, %d\n"%(os.path.basename(problem_path), elapsed, elapsed_sim, success, replans))
+print("%s, %f, %f, %s, %d\n"%(os.path.basename(problem_path), elapsed, elapsed_sim, success, replans))
